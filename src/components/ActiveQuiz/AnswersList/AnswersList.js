40f7a7435d009e597b12f177a9b8cbd163917175
viacheslav <mr.v.e.romantsov@gmail.com>
2020-11-04 18:28:53 +0300

    create jsx for quiz item

---
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from './AnswersList.css'
+import AnswerItem from './AnswerItem/AnswerItem'
+
+const AnswersList = props => (
+  <ul className={classes.AnswersList}>
+    { props.answers.map((answer, index) => (
+      <AnswerItem
+        key={index}
+        answer={answer}
+      />
+    ))}
+  </ul>
+)
+
+export default AnswersList
